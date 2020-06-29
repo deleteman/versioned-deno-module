89b3fd6cd796769858ffc64209dccdbe719c3756
Fernando Doglio <fernando@azlo.com>
2020-06-29 08:25:15 +0200

    adding a simple comment

--- hello.ts
+++ hello.ts
@@ -1,5 +1,8 @@
 import {boldIt, italicIt} from './formatter.ts'
 
+/**
+Adding a simple comment over here
+*/
 export function HelloWorld(name: string, format: string): void {
 	let text = ["Hey there", name, ", say hello to the world!"].join(" ")
 	
